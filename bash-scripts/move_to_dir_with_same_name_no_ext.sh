132cf0a8bb7dccf57f50fdf0fce3cf137cacabe4
joshgaweda <gaweda@gmail.com>
2022-06-06 09:55:11 -0700

    updates compose files and added dockersock

--- bash-scripts/move_to_dir_with_same_name_no_ext.sh
+++ bash-scripts/move_to_dir_with_same_name_no_ext.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for file in /mnt/user/mediadata/media/4k/movies/*; do
+for file in /mnt/user/mediadata/media/temp-processed/*; do
     dir=${file%.mp4}
     mkdir -p "$dir" && mv -i "$file" "$dir"
 done
